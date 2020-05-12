aaf9b7d5a94c76608474cace52d657ad1ab924e6
weidi5858258 <weidi5858258@sina.com>
2020-05-12 11:21:09 +0800

    Add project ffplay

---
+++ cmake-build-debug/CMakeFiles/ffplay.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/alexander/mydev/workspace_github/audio_video/ffmpeg_code/ffplay/cmdutils.c" "/Users/alexander/mydev/workspace_github/audio_video/ffmpeg_code/ffplay/cmake-build-debug/CMakeFiles/ffplay.dir/cmdutils.c.o"
+  "/Users/alexander/mydev/workspace_github/audio_video/ffmpeg_code/ffplay/ffplay_media.c" "/Users/alexander/mydev/workspace_github/audio_video/ffmpeg_code/ffplay/cmake-build-debug/CMakeFiles/ffplay.dir/ffplay_media.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../include"
+  "../src/business/ffplay"
+  "/usr/local/include"
+  "/usr/local/include/lame"
+  "/usr/local/Cellar/sdl2/2.0.10/include/SDL2"
+  "/Users/alexander/mydev/tools/audio_video/ffmpeg_macos/include"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/alexander/mydev/workspace_github/audio_video/ffmpeg_code/ffplay/FFplayer.cpp" "/Users/alexander/mydev/workspace_github/audio_video/ffmpeg_code/ffplay/cmake-build-debug/CMakeFiles/ffplay.dir/FFplayer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../include"
+  "../src/business/ffplay"
+  "/usr/local/include"
+  "/usr/local/include/lame"
+  "/usr/local/Cellar/sdl2/2.0.10/include/SDL2"
+  "/Users/alexander/mydev/tools/audio_video/ffmpeg_macos/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ffplay.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ffplay.dir/FFplayer.cpp.o"
+  "CMakeFiles/ffplay.dir/cmdutils.c.o"
+  "CMakeFiles/ffplay.dir/ffplay_media.c.o"
+  "ffplay"
+  "ffplay.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/ffplay.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
